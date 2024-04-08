44aeb880cfe69b2113bd0bb4196028632d4e70f5
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-04-08 15:54:05 -0400

    Verify Subscription in home page

--- cypress/e2e/subscription.cy.js
+++ cypress/e2e/subscription.cy.js
@@ -7,5 +7,18 @@ describe("Verify Subscription", () => {
       "exist"
     );
   });
-  it("Verify Subscription in home page", () => {});
+  it("Verify Subscription in home page", () => {
+    cy.get("form.searchform")
+      .scrollIntoView({ easing: "linear" })
+      .should("be.visible");
+    cy.get("#footer .single-widget h2")
+      .should("be.visible")
+      .should("contain.text", "Subscription");
+    cy.get("#susbscribe_email").type("neville+test@gmail.com");
+    cy.get("#subscribe").click();
+    //assert
+    cy.get("#footer .form-row .alert")
+      .should("be.visible")
+      .should("contain.text", "You have been successfully subscribed!");
+  });
 });
